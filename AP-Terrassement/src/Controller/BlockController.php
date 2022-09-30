78bbd621e5e53adb20a68ae40e52266cda3ff87c
Hugo Poquet <hugo.poquet@icloud.com>
2022-09-30 15:36:51 +0200

    V1 Init

--- AP-Terrassement/src/Controller/BlockController.php
+++ AP-Terrassement/src/Controller/BlockController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlockController extends AbstractController
 {
     /**
-     * @Route("/block", name="app_block")
+     * @Route("/", name="app_block")
      */
     public function index(): Response
     {
@@ -17,4 +18,36 @@ class BlockController extends AbstractController
             'controller_name' => 'BlockController',
         ]);
     }
+
+    /**
+     * @Route("listPresta", name="listPresta")
+     */
+    public function listPresta(): Response
+    {
+        $repoPresta = $this->getDoctrine()->getRepository(Prestation::class);
+        $prestas = $repoPresta->findAll();
+
+        return $this->render('block/listPresta.html.twig', [
+            'controller_name' => 'BlockController',
+            'prestas' => $prestas
+        ]);
+    }
+
+    /**
+     * @Route("detailsPresta/{id}", name = "detailsPresta")
+     */
+    public function detailsPresta(int $id): Response
+    {
+        $repoPresta = $this->getDoctrine()->getRepository(Prestation::class);
+        $prestas = $repoPresta->findAll();
+
+        // renders templates/block/detailsPresta.html.twig
+        return $this->render('block/detailsPresta.html.twig', [
+            'id' => $id, 
+            'controller_name' => 'BlockController', 
+            'prestas' => $prestas
+        ]);
+    }
 }
+
+?>
