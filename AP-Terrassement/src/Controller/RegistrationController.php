89dae35dba513d801846efc5fe03ccf65918c02b
Vincent-Murienne <muriennevincent@gmail.com>
2022-10-07 17:32:49 +0200

    connec & inscri finish

--- AP-Terrassement/src/Controller/RegistrationController.php
+++ AP-Terrassement/src/Controller/RegistrationController.php
@@ -32,16 +32,12 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
             $entityManager->persist($user);
+            
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+           
         }
 
         return $this->render('registration/register.html.twig', [
